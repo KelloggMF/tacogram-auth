7f0b6c4a84cd1bee01fafba692daf6d7984c79ea
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-31 15:55:02 +0000

    New changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,20 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+     @user = User.find_by({"email" => params["email"]})
+        if @user != nil
+          if BCrypt::Password.new(@user["password"]) == params["password"]
+            session["user_id"] = @user["id"]
+            flash["notice"] = "Welcome."
+            redirect_to "/posts"
+          else
+            flash["notice"] = "Nope."
+            redirect_to "/login"
+          end
+        else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
   end
-end
+  end
+
